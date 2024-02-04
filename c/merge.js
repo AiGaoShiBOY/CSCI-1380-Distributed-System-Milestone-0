1f9a2d331bdfcd567247dde428614086365294be
AiGaoShiBOY <yuanfeng_li@brown.edu>
2024-02-04 23:56:59 +0000

    update merge.js

--- c/merge.js
+++ c/merge.js
@@ -4,16 +4,46 @@
 // the details of the global index can be seen in the test cases.
 
 const fs = require('fs');
-const { exit } = require('process');
+const { exit, mainModule } = require('process');
 const readline = require('readline');
 
 const rl = readline.createInterface({
   input: process.stdin,
 });
 
-// TODO some code here
+const fileName = process.argv[2];
+//first read the current file, and init the global map
+//The current file is stored like ngram | url1 freq url2 freq ... 
+const map = new Map();
+if(fs.existsSync(fileName)){
+  const curContent = fs.readFileSync(fileName, "utf-8");
+  const lineArr = curContent.split("\n").filter(item => item != "");
+  lineArr.forEach(line => {
+    if(!line){
+      return;
+    }
+    const entries = line.split("|").filter(item => item != "");
+    const ngram = entries[0];
+    const urlAndFreq = entries[1];
+    map.set(ngram, urlAndFreq);
+  });
+}
+
 rl.on('line', (line) => {
-  // TODO some code here
+  //parse the new lines from std
+  //new lines should be ngram | freq | url
+  const lineArr = line.split("|").filter(item => item != "");
+  const ngram = lineArr[0];
+  const freq = lineArr[1].trim();
+  const url = lineArr[2].trim(); 
+  //rewrite map
+  if(map.has(ngram)){
+    const curVal = map.get(ngram);
+    const newVal = curVal + " " + url + " " + freq;
+    map.set(ngram, newVal);
+  }else{
+    map.set(ngram, " " + url + " " + freq);
+  }
 });
 
 rl.on('close', () => {
@@ -21,6 +51,14 @@ rl.on('close', () => {
 });
 
 const mergeIndices = () => {
-  // TODO some code here
+  let output = "";
+  map.forEach((value, key) => {
+    lineVal = key + "|" + value;
+    //output to shell
+    console.log(lineVal);
+    output += lineVal + "\n";
+  });
+  fs.writeFileSync(fileName, output);
 }
+
 
